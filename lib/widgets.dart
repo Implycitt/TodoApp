8118f53b142abf55e5ac775768eb8ae7d4b846c8
Implycitt <qgbordelon@gmail.com>
2024-05-31 01:10:13 -0500

    I Have no clue what I'm doing

---
+++ lib/widgets.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer ( 
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Side menu',
+              style: TextStyle(color: Colors.black, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Welcome'),
+            onTap: () => {},
+          ),
+        ],
+      )
+    );
+  }
+
+}
